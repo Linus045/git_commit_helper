32096cded46d452b0d624a1bade48ac966b83c37
Linus045 <lwillms88@gmail.com>
2022-10-29 14:48:37 +0200

    fix: Fixed hook
    Hook now properly edits commit message

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 if ! [[ -f ".git/hooks/prepare-commit-msg" ]]; then
  touch ".git/hooks/prepare-commit-msg"
+ chmod +x ".git/hooks/prepare-commit-msg"
 fi
 
 echo "bash ./linus_commit_helper \$1 \$2 \$3" >> ".git/hooks/prepare-commit-msg"
